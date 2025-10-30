73b2ad3e480c161a0fa78182e8245fa2052a2f12
martin mugo <bralva.co15@gmail.com>
2025-10-30 13:11:12 +0300

    added a body content to my react app

---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function header() {
+    return (
+        <div>
+            <header>
+                <h1>My favourite cities</h1>
+            </header>
+        </div>
+    )
+}
+
+export default header;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent (){
+    return (
+        <div>
+            <main>
+                <p>I love to visit New york, Paris and Tokyo</p>
+            </main>
+        </div>
+    )
+}
+
+export default MainContent;
