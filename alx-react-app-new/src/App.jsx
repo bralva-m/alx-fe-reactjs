1f66c8c7f9374e00dc010d4ccab2c80a9bbb6b83
martin mugo <bralva.co15@gmail.com>
2025-11-12 23:09:31 +0300

    added a counter function to my website

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,9 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/footer'
 import UserProfile from './components/UserProfile'
+import Counter from '.components/Counter'
+
+
 
 function App() {
 
@@ -13,8 +16,11 @@ return (
  <WelcomeMessage />
  <Header />
  <MainContent />
+ <Counter/>
  <Footer />
  <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+ 
+ 
 
  </div>
 )
