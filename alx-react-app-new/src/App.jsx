3566e1dabf34f722547176d4ee6a06b8eb3b614a
martin mugo <bralva.co15@gmail.com>
2025-11-25 21:19:39 +0300

    added counter to my app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 import UserProfile from './components/UserProfile'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-
+import Counter from './components/Counter'
 
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
       <UserProfile />
       <MainContent />
       <Footer />
+      <Counter />
     </>
   )
 }
